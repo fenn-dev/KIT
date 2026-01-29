020a08c3fe35e5d08576aad84eadedd71526dde9
FoxGaming208 <94727250+FoxGaming208@users.noreply.github.com>
2026-01-29 02:04:41 +0100

    Working on the lexer

--- LangCoreAPI.hpp
+++ LangCoreAPI.hpp
@@ -2,6 +2,8 @@
 #include <string_view>
 #include <map>
 
+
+
 class LangCoreAPI {
 public:
     void exec_lexer(std::string_view source);
@@ -12,13 +14,14 @@ public:
     void exec_nasm_generator();
     void exec_assembler();
     void exec_linker();
-
 private:
+    // LEXER DATA
     enum class Token;
     std::map<std::string_view, Token> string_to_token;
-    struct LexerInternals {
-        size_t line = 0;
-        size_t column = 0;
-    };
-	LexerInternals lexer_internals;
+    size_t line = 0;
+    size_t column = 0;
+    size_t cursor = 0;
+    void walk(std::string_view source);
+
+    char advance(std::string_view source);
 };
